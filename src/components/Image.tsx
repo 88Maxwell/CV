a2bc0119c44f7c3d5b854605bc1413ede008e2cc
Soltyk Maksym <maxwell88soltyk@gmail.com>
2024-05-14 20:26:57 +0300

    save

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,13 +1,13 @@
 import NextImage, { ImageProps } from "next/image";
 
 export const Image = ({ src, ...props }: ImageProps): React.ReactNode => {
-  const getSrc = () => {
-    const isProd = process.env.NODE_ENV === "production";
-    const isSrcString = typeof src === "string";
-    if (!isSrcString) return src;
-    if (!isProd) return src;
-    return `/cv${src}`;
-  };
+  // const getSrc = () => {
+  //   const isProd = process.env.NODE_ENV === "production";
+  //   const isSrcString = typeof src === "string";
+  //   if (!isSrcString) return src;
+  //   if (!isProd) return src;
+  //   return `/cv${src}`;
+  // };
 
-  return <NextImage src={getSrc()} {...props} alt={props.alt} />;
+  return <NextImage src={src} {...props} alt={props.alt} />;
 };
