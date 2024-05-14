3eae7b992babeefbf590a531105ef51e8659fffa
Soltyk Maksym <maxwell88soltyk@gmail.com>
2024-05-14 15:52:49 +0300

    save

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
+// const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    images: { unoptimized: true }
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  //   assetPrefix: isProd ? 'https://88maxwell.github.io/cv' : undefined
 };
 
 export default nextConfig;
